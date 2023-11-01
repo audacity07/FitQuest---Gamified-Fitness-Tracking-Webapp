0831d56a09616984453cb6e72a1164b775a463ff
Tapishagrawal <tapishg4@gmail.com>
2023-11-01 20:39:47 +0530

    navbar and landing page complete

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { AnimatePresence, motion } from "framer-motion";
+import styes from "./Navbar.module.css"
+
+const navLinks = [
+    { title: "personal training", href: "/", dataText: "personal training" },
+    { title: "duo training", href: "/", dataText: "duo training" },
+    { title: "fitlab for her", href: "/", dataText: "fitlab for her" },
+    { title: "business", href: "/", dataText: "business" },
+    { title: "yoga", href: "/", dataText: "yoga" },
+    { title: "fitlab at home", href: "/", dataText: "fitlab at home" },
+]
+export const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const toggleMenu = () => {
+        setOpen(prevOpen => !prevOpen)
+    }
+    const menuVars = {
+        initial: {
+            x: "-250vh",
+        },
+        animate: {
+            x: 1,
+            transition: {
+                duration: 0.5,
+                ease: [0.12, 0, 0.39, 0]
+
+            }
+        },
+        exit: {
+            x: "-250vh",
+            transition: {
+                delay: 0.5,
+                duration: 0.5,
+                ease: [0.22, 1, 0.36, 1]
+
+            }
+        }
+    }
+    const containerVars = {
+        initial: {
+            transition: {
+                staggerChildren: 0.09,
+            },
+        },
+        open: {
+            transition: {
+                delayChildren: 0.3,
+                staggerChildren: 0.09
+            }
+        }
+    }
+    return (
+        <header>
+            <nav className='flex justify-between items-center p-10'>
+                <div className='flex items-center gap-[5rem]'>
+                    <div
+                        className='cursor-pointer text-md text-white z-10'
+                        onClick={toggleMenu}
+                    >
+                        Menu
+                    </div>
+                    <div className='flex items-center gap-2 z-[21]'>
+                        <div className='w-5 h-5 bg-yellow-400 rounded-full'>
+                        </div>
+                        <span >
+                            fitlab
+                        </span>
+                    </div>
+                </div>
+            </nav>
+            <AnimatePresence>
+                {
+                    open &&
+                    <motion.div
+                        variants={menuVars}
+                        initial="initial"
+                        animate="animate"
+                        exit="exit"
+                        className='fixed z-20 left-0 top-0 w-full h-screen bg-[#183330] text-white p-10'>
+                        <div className='flex h-full flex-col'>
+                            <div className='flex justify-between'>
+                                <p className='cursor-pointer text-base text-white' onClick={toggleMenu}>Close</p>
+                            </div>
+                            <motion.div
+                                variants={containerVars}
+                                initial="initial"
+                                animate="open"
+                                exit="initial"
+                                className='flex flex-col h-full justify-center items-center gap-4'>
+                                {
+                                    navLinks.map((link, index) => {
+                                        return (
+                                            <div className='overflow-hidden'>
+                                                <MobileNavLink
+                                                    key={index}
+                                                    title={link.title}
+                                                    href={link.href}
+                                                    dataText={link.dataText}
+                                                />
+                                            </div>
+                                        );
+                                    })
+                                }
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                }
+            </AnimatePresence>
+        </header>
+    )
+}
+
+
+const MobileNavLink = ({ title, href, dataText }) => {
+    const mobileLinkVars = {
+        initial: {
+            y: "30vh",
+            transition: {
+                ease: [0.37, 0, 0.63, 1],
+                duration: 0.5
+            }
+        }, open: {
+            y: 0,
+            transition: {
+                ease: [0, 0.55, 0.45, 1],
+                duration: 0.7
+            }
+        }
+
+    }
+    const customStyle = {
+        WebkitTextStroke: "1px #fff"
+    }
+    return (
+        <motion.div variants={mobileLinkVars} >
+            <Link
+                to={href}
+                style={customStyle}
+                dataText={dataText}
+                className={`${styes.mobileLavlink} relative text-5xl uppercase line-clamp-1 text-transparent font-extrabold tracking-widest`}>
+                {title}</Link>
+        </motion.div>
+    )
+}
+
+
+
