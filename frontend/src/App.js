7d216844d00321e20b0ff5f7371c735d2288254c
harsh7739 <hrbhumi123@gmail.com>
2023-11-03 22:13:04 +0530

    day-4 work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import Admin from './components/Admin/admin';
 
 import { AllRoutes } from './components/AllRoutes';
 import { Navbar } from './components/Navbar';
 
 function App() {
+
   return (
     <div >
-      <Navbar/>
-      <AllRoutes/>
+      {/* <Navbar/> */}
+      {/* <AllRoutes/> */}
+      <Admin />
     </div>
   );
 }
